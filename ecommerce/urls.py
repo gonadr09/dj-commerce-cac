47a47c0208c84a72800a47c0b477fad2989b29c8
Luciano Blank <lublank@gmail.com>
2023-04-17 16:23:44 -0300

    compra.html

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import index
+from .views import compra
 
 app_name = 'ecommerce'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('compra', compra, name='compra')
 ]
