e7d67d924440a31acbe1c91c147f05e6d90110bc
Luciano Blank <lublank@gmail.com>
2023-05-19 17:32:55 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cac_ecommerce',
-        'USER': 'postgres',
-        'PASSWORD': 'lucho22postgre',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
