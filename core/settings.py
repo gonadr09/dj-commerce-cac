0b65a71dfed95223b842d7922bbf4ebcd4c5af8d
Luciano Blank <lublank@gmail.com>
2023-05-19 17:30:03 -0300

    modificacion models

--- core/settings.py
+++ core/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cac_ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'lucho22postgre',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -4,13 +4,16 @@ from users.models import CustomUser
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=128, verbose_name="Nombre Categoría")
+    padre = models.ForeignKey('self', related_name='children',
+                              on_delete=models.CASCADE, blank=True, null=True)
+    slug = models.SlugField(max_length=100, unique=True, editable=False)
 
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=128, verbose_name="Nombre Producto")
     stock = models.IntegerField(verbose_name="Cantidad en Stock")
     descripcion = models.CharField(verbose_name="Descripción de Producto")
-    precio = models.IntegerField(verbose_name="Precio")
+    precio = models.DecimalField(max_length=15, decimal_places=2, verbose_name="Precio")
     categoria = models.ForeignKey(Categoria)
     imagen = models.ImageField()
 
@@ -18,4 +21,4 @@ class Producto(models.Model):
 class Compra(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     usuario = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    cantidad = models.IntegerField(verbose_name="Cantiadad Comprada")
+    cantidad = models.PositiveIntegerField(verbose_name="Cantiadad Comprada")
