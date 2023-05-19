98c3a52fd577f3443906240eddd3f3c0751c9740
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-05-19 19:36:56 -0300

    ecommerce models

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -33,7 +33,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=15, decimal_places=2, verbose_name="Precio")
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, verbose_name="Categoría")
     tag = models.ManyToManyField(Tag, blank=True, verbose_name="Etiqueta")
-    image = models.ImageField(upload_to='producto/', verbose_name="Imagen")
     created = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
     updated = models.DateTimeField(auto_now=True, verbose_name="Actualizado")
 
@@ -43,6 +42,18 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='producto/', verbose_name="Imagen")
+
+    class Meta:
+        verbose_name = 'Imagen'
+        verbose_name_plural = 'Imágenes'
+
+    def __str__(self):
+        return self.image
 
 
 class Order(models.Model):
