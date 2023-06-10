89b39e91cbba93d3769c5909fd62f7f57bb6297f
gonadr09 <gonzalo_diaz09@hotmail.com>
2023-06-10 02:10:05 -0300

    Preparing deploy on Render

--- build.sh
+++ build.sh
@@ -6,8 +6,6 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py makemigrations --empty users
 python manage.py makemigrations users
-python manage.py makemigrations --empty ecommerce
 python manage.py makemigrations ecommerce
 python manage.py migrate
